88c43c782ceb112a1fe07794f7f150ca46719dfc
Daniel Hosterman <daniel.hosterman@gmail.com>
2014-08-22 13:40:30 -0500

    correct indentation

--- todo/views.py
+++ todo/views.py
@@ -4,4 +4,10 @@ from django.template import RequestContext
 
 # Create your views here.
 def home(request):
-    return render_to_response('home.html', {}, RequestContext(request))
+    return render_to_response(
+        'home.html',
+        {
+
+        },
+        RequestContext(request)
+    )
