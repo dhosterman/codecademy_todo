2354c5cade0d619906a5243339f2eeb76e0d5e34
Daniel Hosterman <daniel.hosterman@gmail.com>
2014-08-23 01:12:47 -0500

    add form to view

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from .forms import NewTaskForm
 
 
 # Create your views here.
@@ -7,7 +8,7 @@ def home(request):
     return render_to_response(
         'home.html',
         {
-
+            'new_task_form': NewTaskForm()
         },
         RequestContext(request)
     )
