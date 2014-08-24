294ec8d73f558208777433847a0b3d665e70219e
Daniel Hosterman <daniel.hosterman@gmail.com>
2014-08-24 18:43:04 -0500

    create inital hire Daniel task when new user is created

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 # Create your models here.
@@ -7,3 +8,19 @@ class Task(models.Model):
     description = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     owner = models.ForeignKey(User)
+
+
+# signals
+def create_initial_tasks(sender, instance, **kwargs):
+    if kwargs['created']:
+        task = Task(
+            owner=instance,
+            description='Hire Daniel (daniel.hosterman@gmail.com) for some work!'
+        )
+        task.save()
+
+post_save.connect(
+    create_initial_tasks,
+    sender=User,
+    dispatch_uid='create_initial_tasks'
+)
