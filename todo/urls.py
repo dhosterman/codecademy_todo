ba721c7ca0bbfb44b4ce8c500209735863d7265c
Daniel Hosterman <daniel.hosterman@gmail.com>
2014-08-24 04:17:51 -0500

    change from update_task to edit_task

--- todo/urls.py
+++ todo/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     url(r'^complete_task$', 'todo.views.complete_task'),
     url(r'^uncomplete_task$', 'todo.views.uncomplete_task'),
     url(r'^delete_task$', 'todo.views.delete_task'),
-    url(r'^update_task$', 'todo.views.update_task')
+    url(r'^edit_task$', 'todo.views.edit_task')
 )
