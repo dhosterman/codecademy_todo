6055f9e95716ab94ba082e7c6ef4f8dcaf865057
Daniel Hosterman <daniel.hosterman@gmail.com>
2014-08-24 15:35:16 -0500

    formatting login

--- todo/forms.py
+++ todo/forms.py
@@ -17,5 +17,7 @@ class NewTaskForm(forms.ModelForm):
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Username'})
+    )
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
